9b324377d5f4874ff4c7c5de1fd6b18c207cdfa5
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2023-02-04 14:14:55 +0530

    Complete Card View

--- lib/main.dart
+++ lib/main.dart
@@ -13,27 +13,76 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child:
-              Column(
-                children: const [
-                  CircleAvatar(radius: 50,
-                     backgroundImage: NetworkImage("https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584__340.png")
-                  ),
-                Text('Vijayaraghavan S', style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
-                )),
-                  Text('Flutter Developer S', style: TextStyle(
-                      fontFamily: 'SourceSansPro',
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Column(
+              children: [
+                CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(
+                        "https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584__340.png")),
+                Text('Vijayaraghavan S',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
                       fontSize: 30,
                       color: Colors.white,
-                      fontWeight: FontWeight.bold
-                  )),
-                ],
-              ),
+                      fontWeight: FontWeight.bold,
+                    )),
+                Text('Flutter Developer',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                        color: Colors.teal.shade100,
+                        letterSpacing: 2.5)),
+                Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal.shade900,
+                      ),
+                      SizedBox(
+                        width: 20,
+                        height: 40,
+                      ),
+                      Text(
+                        '+91 9980869266',
+                        style: TextStyle(
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 20,
+                            color: Colors.teal.shade900),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.email, color: Colors.teal.shade900),
+                      SizedBox(
+                        width: 20,
+                        height: 40,
+                      ),
+                      Text(
+                        'vijaygenius123@gmail.com',
+                        style: TextStyle(
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 20,
+                            color: Colors.teal.shade900),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
+        ),
       ),
     );
   }
